a14b4c804742ee701aee0db6c6324e0eb4f6be5a
trex32 <akhil.p131@gmail.com>
2017-11-08 09:52:03 +0530

    added savepath

--- App.py
+++ App.py
@@ -5,13 +5,14 @@ import time
 parser = argparse.ArgumentParser(description='Youtube Video Downloader')
 
 parser.add_argument('--url',help='URL to Download', type=str, required=True)
+parser.add_argument('--save',help='Location to save videos/playlists', type=str)
 parser.add_argument('--batch_size', help="No of Concurrent Downloads", type=int, default=4)
 
 args = parser.parse_args()
 
 url = args.url
 batch_size = args.batch_size
+output_dir = args.save
 
-
-downloader = Downloader(url=url, batch_size=batch_size)
+downloader = Downloader(url=url, output_directory = output_dir, batch_size=batch_size)
 downloader.download()
